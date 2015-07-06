7ed3116275194539cd4d1d35427f3db00c2fc764
Michael Deal <mudcube@gmail.com>
2015-07-06 15:23:20 -0700

    exposing methods, improve loading, update notes
    Changes:
    * loadSoundFont* runs in background to improve performance
    * cleanup notes and error logs
    
    New methods:
    * loadSoundFontBank(name, program, msb, lsb);
    * programChange(program, msb, lsb, channel)
    * send(status, data1, data2, delay)
    
    Modified methods:
    * noteOn(note, velocity, channel)
    * noteOff(note, channel)

--- src/ios/CDVInstrument.m
+++ src/ios/CDVInstrument.m
@@ -8,37 +8,24 @@
 
 #import "CDVInstrument.h"
 
-
 @implementation CDVInstrument
 @synthesize engine = _engine;
 
 - (instancetype)init
 {
     self = [super init];
-    if (self) {
-
-
-    }
+    if (self) {}
     return self;
 }
 
 - (void)setup:(CDVInvokedUrlCommand*)command{
-    //create engine
+    // create engine
     self.engine = [[AVAudioEngine alloc] init];
     
-    //create instrument
+    // create instrument
     self.instrument = [[AVAudioUnitSampler alloc] init];
     [self.engine attachNode:self.instrument];
-    
-    //        //load soundfont
-    //        NSBundle *mainBundle = [NSBundle mainBundle];
-    //        NSURL *instrumentURL = [mainBundle URLForResource:@"acoustic_grand_piano" withExtension:@"sf2" subdirectory:@"www"];
-    //        NSString *instrumentPath = [mainBundle pathForResource:@"acoustic_grand_piano" ofType:@"sf2" inDirectory:@"www"];
-    
     [self.engine connect:self.instrument to:self.engine.outputNode format:NULL];
-    //    AVAudioOutputNode *out = [[AVAudioOutputNode alloc] init];
-    //    [self.engine connect:self.engine.outputNode to:out format:NULL];
-    
     [self.engine prepare];
     
     NSError *audioEngineStartError = nil;
@@ -50,52 +37,120 @@
     isInitialised = true;
 }
 
-//Instrument.prototype.loadSoundFont = function(fontName)
+// loadSoundFont(name)
 -(void)loadSoundFont:(CDVInvokedUrlCommand*)command {
-    if(!isInitialised) {
-        [self setup:NULL];
-    }
-    NSString *fontName = [command.arguments objectAtIndex:0];
-    NSBundle *mainBundle = [NSBundle mainBundle];
-    NSURL *instrumentURL = [mainBundle URLForResource:fontName withExtension:@"sf2" subdirectory:@"www/soundfont"];
-    
-    NSError *soundFontLoadError = nil;
-    [self.instrument loadInstrumentAtURL:instrumentURL error:&soundFontLoadError];
-    if (soundFontLoadError) {
-        NSLog(@"Error loading sound font:%@", soundFontLoadError.description);
-    }
-    else {
-        hasSoundFont = true;
-    }
-    NSLog(@"Loaded sound font");
+    [self.commandDelegate runInBackground:^{
+        if (!isInitialised) {
+            [self setup:NULL];
+        }
+        NSString *name = [command.arguments objectAtIndex:0];
+        NSBundle *mainBundle = [NSBundle mainBundle];
+        NSURL *instrumentURL = [mainBundle URLForResource:name withExtension:@"sf2" subdirectory:@"www/soundfont"];
+        NSError *soundFontLoadError = nil;
+        [self.instrument loadInstrumentAtURL:instrumentURL error:&soundFontLoadError];
+        ///
+        NSString *payload = nil;
+        if (soundFontLoadError) {
+            payload = @"Error loading sound font:%@", soundFontLoadError.description;
+        } else {
+            payload = @"Loaded sound font";
+            hasSoundFont = true;
+        }
+        NSLog(payload);
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
-//Instrument.prototype.noteOn = function(noteNumber, velocity, successCallback, errorCallback)
+// loadSoundFontBank(name, program, bankMSB, bankLSB)
+-(void)loadSoundFontBank:(CDVInvokedUrlCommand*)command {
+    [self.commandDelegate runInBackground:^{
+        if (!isInitialised) {
+            [self setup:NULL];
+        }
+        NSString *name = [command.arguments objectAtIndex:0];
+        Byte program = [[command.arguments objectAtIndex:1] intValue];
+        Byte bankMSB = [[command.arguments objectAtIndex:2] intValue];
+        Byte bankLSB = [[command.arguments objectAtIndex:3] intValue];
+        NSBundle *mainBundle = [NSBundle mainBundle];
+        NSURL *instrumentURL = [mainBundle URLForResource:name withExtension:@"sf2" subdirectory:@"www/soundfont"];
+        NSError *soundFontLoadError = nil;
+        [self.instrument loadSoundBankInstrumentAtURL:instrumentURL program:program bankMSB:bankMSB bankLSB:bankLSB error:&soundFontLoadError];
+        ///
+        NSString *payload = nil;
+        if (soundFontLoadError) {
+            payload = @"Error loading sound font:%@", soundFontLoadError.description;
+        } else {
+            payload = @"Loaded sound font";
+            hasSoundFont = true;
+        }
+        NSLog(payload);
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+// programChange(program, bankMSB, bankLSB, channel)
+- (void)programChange:(CDVInvokedUrlCommand*)command {
+    if (hasSoundFont) {
+        Byte program = [[command.arguments objectAtIndex:0] intValue];
+        Byte bankMSB = [[command.arguments objectAtIndex:1] intValue];
+        Byte bankLSB = [[command.arguments objectAtIndex:2] intValue];
+        Byte channel = [[command.arguments objectAtIndex:3] intValue];
+        [self.instrument sendProgramChange:program bankMSB:bankMSB bankLSB:bankLSB onChannel:channel];
+    } else {
+        NSLog(@"No sound font loaded. Cannot use programChange");
+    }
+}
+
+// send(status, data1, data2, delay)
+- (void)send:(CDVInvokedUrlCommand*)command {
+    if (hasSoundFont) {
+        Byte status = [[command.arguments objectAtIndex:0] intValue];
+        Byte data1 = [[command.arguments objectAtIndex:1] intValue];
+        Byte data2 = [[command.arguments objectAtIndex:2] intValue];
+        double delay = [[command.arguments objectAtIndex:3] doubleValue];
+        if (delay) {
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [self.instrument sendMIDIEvent:status data1:data1 data2:data2];
+            });
+        } else {
+            [self.instrument sendMIDIEvent:status data1:data1 data2:data2];
+        }
+    }
+    else {
+        NSLog(@"No sound font loaded. Cannot send");
+    }
+}
+
+// noteOn(noteNumber, velocity)
 - (void)noteOn:(CDVInvokedUrlCommand*)command {
     if (hasSoundFont) {
-        int noteNumber = [[command.arguments objectAtIndex:0] intValue];
-        int velocity = [[command.arguments objectAtIndex:1] intValue];
+        Byte noteNumber = [[command.arguments objectAtIndex:0] intValue];
+        Byte velocity = [[command.arguments objectAtIndex:1] intValue];
+        Byte channel = [[command.arguments objectAtIndex:2] intValue];
         NSLog(@"Playing Note %i with velocity %i", noteNumber, velocity);
-        [self.instrument startNote:noteNumber withVelocity:velocity onChannel:0];
-    }
-    else {
-        NSLog(@"No sound font loaded. Cannot play note");
+        [self.instrument startNote:noteNumber withVelocity:velocity onChannel:channel];
+    } else {
+        NSLog(@"No sound font loaded. Cannot use noteOn");
     }
 }
 
-//Instrument.prototype.noteOff = function(noteNumber, successCallback, errorCallback)
+// noteOff(noteNumber)
 - (void)noteOff:(CDVInvokedUrlCommand*)command {
     if (hasSoundFont) {
-        int noteNumber = [[command.arguments objectAtIndex:0] intValue];
+        Byte noteNumber = [[command.arguments objectAtIndex:0] intValue];
+        Byte channel = [[command.arguments objectAtIndex:1] intValue];
         NSLog(@"Stopping Note %i", noteNumber);
-        [self.instrument stopNote:noteNumber onChannel:0];
-    }
-    else {
-        NSLog(@"No sound font loaded. Cannot play note");
+        [self.instrument stopNote:noteNumber onChannel:channel];
+    } else {
+        NSLog(@"No sound font loaded. Cannot use noteOff");
     }
     
 }
 
+// expression(expression)
 - (void)expression:(CDVInvokedUrlCommand*)command {
     if (hasSoundFont) {
         int expression = [[command.arguments objectAtIndex:0] intValue];
@@ -106,6 +161,5 @@
         NSLog(@"No sound font loaded. Cannot change expression");
     }
 }
-
 
 @end
